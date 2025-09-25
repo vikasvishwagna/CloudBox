001e2ee99b85108e93f5e405c9145cba1f3afc3c
vikasvishwagna <vishwagnavikassai@gmail.com>
2025-09-26 01:51:44 +0530

    feat:created user registration page

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const userRouter = require('./routes/user.routes');
 
 app.set('view engine', 'ejs');
 
+app.use(express.json());
+app.use(express.urlencoded({extended : true}))
+
 app.use('/user', userRouter);
 
 
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/router", (req, res) => {
-  res.send('this is router folder');
-});
+router.get('/register', (req,res)=>{
+  res.render('register')
+})
 
+
+router.post('/register', (req, res)=>{
+  console.log(req.body);
+  res.send("User data received");
+})
 
 module.exports = router
